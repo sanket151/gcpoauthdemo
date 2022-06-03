d4a26aa7cf1df7010b1509a3c9fdfa1c53880479
sanket <sanket151@aol.com>
2022-06-03 17:00:14 +0530

    first commit

---
+++ src/main/java/com/example/gcpoauthdemo/GcpoauthdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcpoauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpoauthdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpoauthdemoApplication.class, args);
+	}
+
+}
